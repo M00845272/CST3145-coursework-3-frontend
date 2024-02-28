df58e46eec8cedbf1d1a8fe29084d86be70a93b2
M00845272 <SL1558@live.mdx.ac.uk>
2024-02-28 00:06:56 +0000

    HelloWorld SFC app

---
+++ lesson-cart/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+      <header>
+          <h1>{{ sitename }}</h1>
+          <button @click="showCheckout">{{ this.cart.length }}
+              Checkout</button>
+      </header>
+      <main>
+          <component :is="currentView"></component>
+      </main>
+  </div>
+</template>
+<script>
+import LessonList from "./components/LessonList.vue";
+import Checkout from "./components/Checkout.vue";
+export default {
+  name: "App",
+  data() {
+      return {
+          sitename: "Vue.js Pet Depot",
+          cart: [],
+          currentView: LessonList
+      }
+  },
+  components: { LessonList, Checkout },
+  methods: {
+      showCheckout() {
+          if (this.currentView === LessonList) return true;
+          return false;
+      }
+  }
+};
+</script>
---
+++ lesson-cart/src/components/LessonList.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Welcome to the Lesson List Page</div>
+</template>
